f78bf0d99643bd22b1378c7ee69c01e6c031d837
Barhon Moshe Dasa <moshebdasa@gmail.com>
2025-05-01 11:41:23 +0300

    devTools: false,

--- src/utils/window-manager.ts
+++ src/utils/window-manager.ts
@@ -22,6 +22,7 @@ export function createNotificationWindow(): BrowserWindow {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
+      devTools: false,
     },
   });
 
